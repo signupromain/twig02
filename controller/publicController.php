7dc7a787a6cbfa4e030eb44c870ed5795359dc6c
signupromain <romain@machaharson.com>
2018-09-28 09:49:33 +0200

    email:tada:

--- controller/publicController.php
+++ controller/publicController.php
@@ -1,28 +1,54 @@
 <?php
-
+// name of our namspace
 namespace Controller;
-
+// use nosModels
 use Models\nosModels AS DT;
-
-class publicController{
-
-        public static function welcomeAction($twig){
-            $datas = DT::accueilDatas();
-            echo $twig->render("accueil.html.twig", ["recup"=>$datas]);
-        }
-
-        public static function contactAction($twig){
-            if(!empty($_post)){
-                $bool = DT::envoieMail($_POST);
-                if($bool){
-                    header ("Location: ./");
-            }else{
-                    header ("Location: ./?content=contact&erreur=true");
-                }
-
+// Swiftmailer
+use Swift_SmtpTransport;
+use Swift_Mailer;
+use Swift_Message;
+// class for publicController
+class publicController {
+    // method for "acceuil"
+    public static function welcomeAction($twig) {
+        // recup datas from model
+        $datas = DT::accueilDatas();
+        // view accueil with $datas in an associative array, with key "recup"
+        echo $twig->render("accueil.html.twig", ["recup" => $datas]);
+    }
+    // method for "contact"
+    public static function contactAction($twig) {
+        // pas de formulaire envoyé
+        if (!empty($_POST)) {
+            $themail = filter_var($_POST['themail'], FILTER_VALIDATE_EMAIL);
+            $thetext = strip_tags($_POST['thetext']);
+            $aqui = "romain@machaharson.com";
+            // Create the Transport
+            $transport = (new Swift_SmtpTransport('relay.skynet.be', 25))
+                ->setUsername('romain')
+                ->setPassword('')
+            ;
+// Create the Mailer using your created Transport
+            $mailer = new Swift_Mailer($transport);
+// Create a message
+            $message = (new Swift_Message('Depuis twig02'))
+                ->setFrom([$themail])
+                ->setTo([$aqui])
+                ->setBody($thetext)
+            ;
+// Send the message
+            $result = $mailer->send($message);
+            // si ça a réussi
+            if ($result) {
+                header("Location: ./");
+            } else {
+                header("Location: ./?content=contact&erreur=true");
             }
+            // affichage du formulaire
+        } else {
+            // recup form
             $datas = DT::formDatas();
-            echo $twig->render("form.html.twig", ["recup"=>$datas]);
-
+            echo $twig->render("form.html.twig", ["recup" => $datas]);
+        }
     }
 }
--- models/nosModels.php
+++ models/nosModels.php
@@ -21,18 +21,4 @@ class nosModels
     {
         return ['content' => '<form action="" name="lui" method="post"><input name="themail" type="email" placeholder="Votre mail" required><br/><textarea name="thetext" placeholder="Votre message" required></textarea><br><input type="submit" value="Envoyer"></form> '];
     }
-
-    public static function envoieMail(Array $datas){
-    $themail= filter_var($_POST['themail'], FILTER_VALIDATE_EMAIL);
-    $thetext= htmlentities(strip_tags($_POST['thetext']), ENT_QUOTES);
-    $aqui = "romain@machaharson.com";
-
-        if(@mail($aqui,"test de notre site",$thetext,'From: ' .$aqui. "\r\n" .
-'Reply-To: '.$aqui. "\r\n" .
-'X-Mailer: PHP/' . phpversion())){
-        return true;
-    }else{
-return false;
-    }
-    }
 }
